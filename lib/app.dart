86eaa9a300ade863c818916f0f4abbcc7ba184df
Hendrikson Petzold <hendrikson_14@hotmail.com>
2023-02-15 16:07:43 -0300

    feat:routes

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,20 @@
+import 'package:challange_mobile_developer_flutter/config/initial_bindings.dart';
+import 'package:challange_mobile_developer_flutter/config/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      initialBinding: InitialBindings(),
       title: 'Challenge Mobile Developer Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      getPages: Routes.routes,
     );
   }
 }
---
+++ lib/config/routes.dart
@@ -0,0 +1,17 @@
+import 'package:challange_mobile_developer_flutter/presenter/pages/initial/initial_page.dart';
+import 'package:challange_mobile_developer_flutter/presenter/pages/initial/initial_page_controller.dart';
+import 'package:get/get.dart';
+
+class Routes {
+  static final List<GetPage> routes = [
+    GetPage(
+      name: '/',
+      page: () => const InitialPage(),
+      binding: BindingsBuilder(
+        () {
+          Get.put(InitialPageController());
+        },
+      ),
+    )
+  ];
+}
