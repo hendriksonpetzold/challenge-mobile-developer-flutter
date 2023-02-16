c20764ddf5c3982191976b986f7e74e19ea696df
Hendrikson Petzold <hendrikson_14@hotmail.com>
2023-02-16 16:30:46 -0300

    fix:remove expanded on select movie type

--- lib/presenter/pages/home/components/home_page_select_movie_type_bar.dart
+++ lib/presenter/pages/home/components/home_page_select_movie_type_bar.dart
@@ -10,56 +10,54 @@ class HomePageSelectMovieTypeBar extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 40,
-      child: Expanded(
-        child: Obx(
-          () {
-            return ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: controller.genres.length + 1,
-              itemBuilder: (context, index) {
-                final movieTypeEnum = controller.movieTypes[index];
-                if (index == 0) {
-                  return Obx(() {
-                    return AppTextButton(
-                      isSelected: controller.checkActiveList(
-                        newMovieType: movieTypeEnum,
-                      ),
-                      padding: const EdgeInsets.only(
-                        bottom: 4,
-                        left: 8,
-                        right: 8,
-                      ),
-                      label: 'All',
-                      onTap: () {
-                        controller.changeList(newMovieType: movieTypeEnum);
-                        controller.fetchMoviesByGenre('All');
-                      },
-                    );
-                  });
-                } else {
-                  final genre = controller.genres[index - 1];
-                  return Obx(() {
-                    return AppTextButton(
-                      isSelected: controller.checkActiveList(
-                        newMovieType: movieTypeEnum,
-                      ),
-                      padding: const EdgeInsets.only(
-                        bottom: 4,
-                        left: 8,
-                        right: 8,
-                      ),
-                      label: genre.name,
-                      onTap: () {
-                        controller.changeList(newMovieType: movieTypeEnum);
-                        controller.fetchMoviesByGenre(genre.name);
-                      },
-                    );
-                  });
-                }
-              },
-            );
-          },
-        ),
+      child: Obx(
+        () {
+          return ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: controller.genres.length + 1,
+            itemBuilder: (context, index) {
+              final movieTypeEnum = controller.movieTypes[index];
+              if (index == 0) {
+                return Obx(() {
+                  return AppTextButton(
+                    isSelected: controller.checkActiveList(
+                      newMovieType: movieTypeEnum,
+                    ),
+                    padding: const EdgeInsets.only(
+                      bottom: 4,
+                      left: 8,
+                      right: 8,
+                    ),
+                    label: 'All',
+                    onTap: () {
+                      controller.changeList(newMovieType: movieTypeEnum);
+                      controller.fetchMoviesByGenre('All');
+                    },
+                  );
+                });
+              } else {
+                final genre = controller.genres[index - 1];
+                return Obx(() {
+                  return AppTextButton(
+                    isSelected: controller.checkActiveList(
+                      newMovieType: movieTypeEnum,
+                    ),
+                    padding: const EdgeInsets.only(
+                      bottom: 4,
+                      left: 8,
+                      right: 8,
+                    ),
+                    label: genre.name,
+                    onTap: () {
+                      controller.changeList(newMovieType: movieTypeEnum);
+                      controller.fetchMoviesByGenre(genre.name);
+                    },
+                  );
+                });
+              }
+            },
+          );
+        },
       ),
     );
   }
