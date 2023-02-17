eada7463d5e02ecdc19c8e171193847e072a19b7
Hendrikson Petzold <hendrikson_14@hotmail.com>
2023-02-17 12:34:25 -0300

    refact: ci

--- test/app_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:challange_mobile_developer_flutter/app.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('app test', (tester) async {
-    tester.pumpWidget(const App());
-  });
-}
