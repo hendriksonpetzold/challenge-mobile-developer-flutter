74c7bad0a0612736e82f58ab024e3a0e803a6356
Hendrikson Petzold <hendrikson_14@hotmail.com>
2023-02-17 11:04:53 -0300

    feat:get_movie_mock

---
+++ test/utils/mocks/get_movies_mock.dart
@@ -0,0 +1,39 @@
+Map<String, dynamic> moviesMock = {
+  'page': 1,
+  'results': [
+    {
+      "adult": false,
+      "backdrop_path": "/xDMIl84Qo5Tsu62c9DGWhmPI67A.jpg",
+      "genre_ids": [28, 12, 878],
+      "id": 505642,
+      "original_language": "en",
+      "original_title": "Black Panther: Wakanda Forever",
+      "overview":
+          "Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T’Challa’s death.  As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia and Everett Ross and forge a new path for the kingdom of Wakanda.",
+      "popularity": 4646.778,
+      "poster_path": "/sv1xJUazXeYqALzczSZ3O6nkH75.jpg",
+      "release_date": "2022-11-09",
+      "title": "Black Panther: Wakanda Forever",
+      "video": false,
+      "vote_average": 7.5,
+      "vote_count": 3132
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/9Rq14Eyrf7Tu1xk0Pl7VcNbNh1n.jpg",
+      "genre_ids": [28, 12, 53],
+      "id": 646389,
+      "original_language": "en",
+      "original_title": "Plane",
+      "overview":
+          "After a heroic job of successfully landing his storm-damaged aircraft in a war zone, a fearless pilot finds himself between the agendas of multiple militias planning to take the plane and its passengers hostage.",
+      "popularity": 3876.073,
+      "poster_path": "/2g9ZBjUfF1X53EinykJqiBieUaO.jpg",
+      "release_date": "2023-01-13",
+      "title": "Plane",
+      "video": false,
+      "vote_average": 6.7,
+      "vote_count": 412
+    },
+  ]
+};
