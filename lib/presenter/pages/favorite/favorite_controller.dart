70dd46e0b99a1fe9c16c5026e27b164747ce9456
Hendrikson Petzold <hendrikson_14@hotmail.com>
2023-02-20 14:32:10 -0300

    feat: msg on favorite page when you dont have favorities

--- lib/presenter/pages/favorite/favorite_controller.dart
+++ lib/presenter/pages/favorite/favorite_controller.dart
@@ -2,9 +2,14 @@ import 'package:challange_mobile_developer_flutter/domain/entities/movie_entity.
 import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 
+import '../../../domain/entities/genre_entity.dart';
+import '../../../domain/usecases/get_genre/get_genre_usecase.dart';
+
 class FavoriteController extends GetxController {
+  final GetGenreUsecase getGenreUsecase = Get.find<GetGenreUsecase>();
   late Box<MovieEntity> favoriteMovieBox;
   RxList<MovieEntity> favoriteMovies = RxList([]);
+  final RxList<GenreEntity> genres = RxList([]);
   @override
   void onInit() {
     favoriteMovieBox = Hive.box('favorities');
@@ -16,5 +21,10 @@ class FavoriteController extends GetxController {
 
   void updateList() {
     favoriteMovies.value = favoriteMovieBox.values.toList().cast<MovieEntity>();
+  }
+
+  Future<void> fetchGenres() async {
+    final result = await getGenreUsecase.execute();
+    result.fold((l) => l, (r) => genres.addAll(r));
   }
 }
--- lib/presenter/pages/favorite/favorite_page.dart
+++ lib/presenter/pages/favorite/favorite_page.dart
@@ -33,41 +33,61 @@ class FavoritePage extends GetView<FavoriteController> {
             const SizedBox(
               height: 16,
             ),
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 200,
-                  childAspectRatio: 2 / 3,
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
-                ),
-                itemCount: controller.favoriteMovies.length,
-                itemBuilder: (context, index) {
-                  final favoriteMovie = controller.favoriteMovies[index];
-                  return AppMovieCard(
-                    image: favoriteMovie.image,
-                    movieName: favoriteMovie.name,
-                    tag: 'favorite$index',
-                    onAddIconTap: () {
-                      Get.toNamed(
-                        '/movie_detail',
-                        arguments: {
-                          'movieId': favoriteMovie.id,
-                          'movieImage': favoriteMovie.image,
-                          'movieTitle': favoriteMovie.name,
-                          'movieOverview': favoriteMovie.overview,
-                          'releaseDate': favoriteMovie.releaseDate,
-                          'voteAverage': favoriteMovie.voteAverage,
-                          'genreIds': favoriteMovie.genreIds,
-                          'isFavorite': true,
-                          'tag': 'favorite$index'
+            Obx(() {
+              if (controller.favoriteMovies.isNotEmpty) {
+                return Expanded(
+                  child: Obx(
+                    () {
+                      return GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithMaxCrossAxisExtent(
+                          maxCrossAxisExtent: 200,
+                          childAspectRatio: 2 / 3,
+                          crossAxisSpacing: 16,
+                          mainAxisSpacing: 16,
+                        ),
+                        itemCount: controller.favoriteMovies.length,
+                        itemBuilder: (context, index) {
+                          final favoriteMovie =
+                              controller.favoriteMovies[index];
+                          return AppMovieCard(
+                            image: favoriteMovie.image,
+                            movieName: favoriteMovie.name,
+                            tag: 'favorite$index',
+                            onAddIconTap: () {
+                              Get.toNamed(
+                                '/movie_detail',
+                                arguments: {
+                                  'movieId': favoriteMovie.id,
+                                  'movieImage': favoriteMovie.image,
+                                  'movieTitle': favoriteMovie.name,
+                                  'movieOverview': favoriteMovie.overview,
+                                  'releaseDate': favoriteMovie.releaseDate,
+                                  'voteAverage': favoriteMovie.voteAverage,
+                                  'genreIds': favoriteMovie.genreIds,
+                                  'genres': controller.genres,
+                                  'isFavorite': true,
+                                  'tag': 'favorite$index'
+                                },
+                              );
+                            },
+                          );
                         },
                       );
                     },
-                  );
-                },
-              ),
-            ),
+                  ),
+                );
+              } else {
+                return const Text(
+                  'You haven\'t added any favorites yet',
+                  style: TextStyle(
+                    color: AppColors.textColor,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                );
+              }
+            })
           ],
         ),
       ),
